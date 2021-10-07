2a5d5d832179164c841bc0e22c468728261902b2
Bruce <cmvubele@gmail.com>
2021-10-07 19:51:14 +0200

    adding documentation to functions

--- stack.py
+++ stack.py
@@ -11,19 +11,54 @@ class Stack():
         self.items = []
 
     def push(self,item):
+        ''' 
+        Appends item to top of stack
+
+        PARAMETER:
+        Item to append to stack
+
+        ---------------------
+
+        Returns:
+        Stack with appended item
+        '''
         self.items.append(item)
 
     def pop(self):
+        '''
+        Removes and returns item at top of stock
+
+        '''
         return self.items.pop()
 
     def is_empty(self):
+        '''
+        Checks if stack is empty
+        ------------------------
+        Returns:
+        bool value
+        '''
         return self.items == []
 
     def peek(self):
-        if s.is_empty() == False:
+        '''
+        View the item at the top of the Stack
+
+        --------------------------
+        Returns:
+        Top element of Stack
+        '''
+        if s.is_empty() == True:
             return self.items[-1]
 
+        else:
+            print('Stack is empty')
+
     def get_stack(self):
+        '''
+        Returns:
+        Stack
+        '''
         return self.items
 
 
@@ -32,4 +67,10 @@ if __name__ == '__main__':
     s = Stack()
     s.push('a')
     s.push('b')
-    print(s.peek())
+    #print(s.peek())
+    #print(s.get_stack())
+
+    c = Stack()
+    print(c.is_empty())
+    print(c.get_stack())
+    c.peek()
