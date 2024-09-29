5f5cdb12c33f75753abcc99feb12ccaf5c2e076e
Bruce Mvubele <cmvubele@gmail.com>
2024-09-29 13:52:17 +0200

    updated stack

--- stack.py
+++ stack.py
@@ -5,31 +5,63 @@ Implemntation of the Stack data stucture
 @author Bruce Mvubele
 '''
 
+class Node:
+    def __init__(self,data = None,next=None) -> None:
+        self.data = data
+        self.next = next
+
+
 
 class Stack():
     def __init__(self):
-        self.items = []
+        self.top = None
+        self.length = 0
 
-    def push(self,item):
-        ''' 
-        Appends item to top of stack
-
-        PARAMETER:
-        Item to append to stack
-
-        ---------------------
-
-        Returns:
-        Stack with appended item
+    def get_stack(self)-> str:
         '''
-        self.items.append(item)
+        Returns stack as a string
+        ------------------------
+        
+        '''
+        stack_ = ''
+        temp_node = self.top
+        while temp_node:
+            stack_ += str(temp_node.data) +'-->'
+            temp_node = temp_node.next
+        print(stack_)
+        return stack_
+
+    def push(self,data):
+        '''
+        Adds Node with data to the stack
+        -------------------------------
+        returns:
+        None
+        '''
+        new_node = Node(data)
+        if self.top is None:
+            self.top = new_node
+            self.length += 1
+        else:
+            new_node.next = self.top
+            self.top = new_node
+            self.length += 1
+        return
 
     def pop(self):
         '''
         Removes and returns item at top of stock
-
+        --------------------------------------
+        Returns:
+        Top node
         '''
-        return self.items.pop()
+        curr_node = self.top
+        if curr_node is None:
+            print('Stack is empty')
+            return 'Stack is empty'
+        else:
+            self.top = curr_node.next
+        return curr_node
 
     def is_empty(self):
         '''
@@ -38,7 +70,10 @@ class Stack():
         Returns:
         bool value
         '''
-        return self.items == []
+        if self.top is None:
+            return True
+        else:
+            return False
 
     def peek(self):
         '''
@@ -48,29 +83,24 @@ class Stack():
         Returns:
         Top element of Stack
         '''
-        if s.is_empty() == True:
-            return self.items[-1]
+        if s.is_empty() == False:
+            print(self.top.data)
+            return self.top.data
 
         else:
             print('Stack is empty')
 
-    def get_stack(self):
-        '''
-        Returns:
-        Stack
-        '''
-        return self.items
-
-
-
 if __name__ == '__main__':
     s = Stack()
-    s.push('a')
-    s.push('b')
-    #print(s.peek())
-    #print(s.get_stack())
+    s.peek()
+    print(s.is_empty())
+    s.push(1)
+    s.push('new')
+    s.push('fresh')
+    s.get_stack()
+    print(s.length)
+    s.pop()
+    print(s.is_empty())
+    s.get_stack()
+    s.peek()
 
-    c = Stack()
-    print(c.is_empty())
-    print(c.get_stack())
-    c.peek()
